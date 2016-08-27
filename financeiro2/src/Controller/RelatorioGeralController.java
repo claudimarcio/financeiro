e594c9b7117ff1f97f65f76aec26d80c16966b30
claudimarcio <claudimarcio@claudimarcioPC>
2016-08-27 14:22:22 -0300

    relatorio Geral

---
+++ financeiro2/src/Controller/RelatorioGeralController.java
@@ -0,0 +1,233 @@
+package Controller;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.servlet.http.HttpSession;
+
+import model.CadastroDespesasModel;
+import model.RelatorioGeralModel;
+import DAO.CadastroDespesasDAO;
+import DAO.CadastroReceitasDAO;
+import DAO.DataDao;
+import DAO.DespesasDAO;
+import DAO.UsuariosDAO;
+
+@ManagedBean(name = "relatorioGeralController")
+@SessionScoped
+public class RelatorioGeralController {
+
+	private Date dataInicio, dataFinal;
+	private boolean mudacorCampoSaldoAtual;
+	private DataDao dataDao;
+	private DataModel<CadastroDespesasDAO> listdespesaData;
+	private DataModel<CadastroReceitasDAO> listreceitaData;
+	private DataModel<CadastroReceitasDAO> listreceitasData;
+	private DespesasDAO despesaDao;
+	private CadastroDespesasDAO cadastroDespeDAO;
+	private String valorTotalDespesaStr;
+	private String valorTotalReceitaStr;
+	private Double valorTotalDespesaDoub;
+	private Double valorTotalReceitaDoub;
+	private String diferencaDespesaReceitastr;
+	CadastroDespesasModel cadastrodespM = new CadastroDespesasModel();
+	RelatorioGeralModel relatorioGeral = new RelatorioGeralModel();
+
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+
+	public DataModel<CadastroReceitasDAO> getListreceitaData() {
+		return listreceitaData;
+	}
+
+	public void setListreceitaData(
+			DataModel<CadastroReceitasDAO> listreceitaData) {
+		this.listreceitaData = listreceitaData;
+	}
+
+	public DataModel<CadastroDespesasDAO> getListdespesaData() {
+		return listdespesaData;
+	}
+
+	public void setListdespesaData(
+			DataModel<CadastroDespesasDAO> listdespesaData) {
+		this.listdespesaData = listdespesaData;
+	}
+	public String getValorTotalDespesaStr() {
+		return valorTotalDespesaStr;
+	}
+
+	public void setValorTotalDespesaStr(String valorTotalDespesaStr) {
+		this.valorTotalDespesaStr = valorTotalDespesaStr;
+	}
+	
+	
+	public String getValorTotalReceitaStr() {
+		return valorTotalReceitaStr;
+	}
+
+	public void setValorTotalReceitaStr(String valorTotalReceitaStr) {
+		this.valorTotalReceitaStr = valorTotalReceitaStr;
+	}
+	
+	
+	public Double getValorTotalDespesaDoub() {
+		return valorTotalDespesaDoub;
+	}
+
+	public void setValorTotalDespesaDoub(Double valorTotalDespesaDoub) {
+		this.valorTotalDespesaDoub = valorTotalDespesaDoub;
+	}
+	
+	public String getDiferencaDespesaReceitastr() {
+		return diferencaDespesaReceitastr;
+	}
+
+	public void setDiferencaDespesaReceitastr(String diferencaDespesaReceitastr) {
+		this.diferencaDespesaReceitastr = diferencaDespesaReceitastr;
+	}
+
+
+	
+	
+
+	public void listaDespesaDataInicioFinal() throws ParseException {
+
+		System.out.println("até aqui chegou");
+		
+		
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext()
+				.getSession(false);
+
+		UsuariosDAO usuario1 = (UsuariosDAO) session.getAttribute("usuario");
+		long usuarioId = usuario1.getId();
+
+		List<CadastroDespesasDAO> lista = new ArrayList<CadastroDespesasDAO>();
+		lista = relatorioGeral.gerenciaListaDespesa(usuarioId, dataInicio,
+				dataFinal);
+
+		listdespesaData = new ListDataModel<CadastroDespesasDAO>(lista);
+
+	
+
+	}
+
+	public void listaReceitaDataInicioFinal() throws ParseException {
+
+	
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext()
+				.getSession(false);
+
+		UsuariosDAO usuario1 = (UsuariosDAO) session.getAttribute("usuario");
+		long usuarioId = usuario1.getId();
+
+		List<CadastroReceitasDAO> lista = new ArrayList<CadastroReceitasDAO>();
+		lista= relatorioGeral.gerenciaListaReceita(usuarioId, dataInicio,
+				dataFinal);
+
+		listreceitaData = new ListDataModel<CadastroReceitasDAO>(lista);
+
+		
+	}
+	
+	public void valorTotalDespesas() throws ParseException {
+
+		
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext()
+				.getSession(false);
+
+		UsuariosDAO usuario1 = (UsuariosDAO) session.getAttribute("usuario");
+		long usuarioId = usuario1.getId();
+
+		
+		valorTotalDespesaDoub= relatorioGeral.relatorioGeralDespesaTotal(usuarioId, dataInicio,
+				dataFinal);
+
+		 setValorTotalDespesaStr(NumberFormat.getCurrencyInstance().format(valorTotalDespesaDoub));
+		
+        
+	}
+	
+public void valorTotalReceitas() throws ParseException {
+
+		
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext()
+				.getSession(false);
+
+		UsuariosDAO usuario1 = (UsuariosDAO) session.getAttribute("usuario");
+		long usuarioId = usuario1.getId();
+
+		
+		valorTotalReceitaDoub= relatorioGeral.relatorioGeralreceitaTotal(usuarioId, dataInicio,
+				dataFinal);
+
+		 setValorTotalReceitaStr(NumberFormat.getCurrencyInstance().format(valorTotalReceitaDoub));
+		
+        
+	}
+
+
+  public void diferencaDespesaReceita(){
+	  
+	  double valorDiferenca = valorTotalReceitaDoub-valorTotalDespesaDoub;
+	  
+	//  diferencaDespesaReceitastr = NumberFormat.getCurrencyInstance().format(valorDiferenca);
+	 
+	  setDiferencaDespesaReceitastr(NumberFormat.getCurrencyInstance().format(valorDiferenca));
+	  
+  }
+	
+	
+	
+	public void ChamaMetodoDespesaEReceita() throws ParseException{
+		
+      listaDespesaDataInicioFinal();
+      listaReceitaDataInicioFinal();
+      valorTotalDespesas();
+      valorTotalReceitas();
+      diferencaDespesaReceita();
+		
+	}
+
+	public void reset() {
+		dataInicio = null;
+		dataFinal = null;
+	}
+
+	
+	
+	
+	
+	
+	
+
+}
---
+++ financeiro2/src/model/RelatorioGeralModel.java
@@ -0,0 +1,201 @@
+package model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import DAO.CadastroDespesasDAO;
+import DAO.CadastroReceitasDAO;
+import conexao.PersistenceUtil;
+
+public class RelatorioGeralModel {
+	
+	
+	
+	//select EXTRACT(YEAR_MONTH FROM cadastro_despesas_data) as data, sum(cadastro_despesas_valor) from cadastro_despesas group by EXTRACT(YEAR_MONTH FROM cadastro_despesas_data);
+	//select month(cadastro_despesas_data),Year(cadastro_despesas_data), sum(cadastro_despesas_valor) from cadastro_despesas group by YEAR(cadastro_despesas_data), month(cadastro_despesas_data);
+	
+	
+	public List<CadastroDespesasDAO> gerenciaListaDespesa(Long id,
+			Date dataInicio, Date datafinal) throws ParseException {
+
+		EntityManager em = PersistenceUtil.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		List<CadastroDespesasDAO> lista = null;
+
+		// formatando a data
+		SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd");
+String data10= formato.format(dataInicio);
+		SimpleDateFormat formato1 = new SimpleDateFormat("yyyy/MM/dd");
+String data11=formato1.format(datafinal);
+				
+		try {
+			tx.begin();
+			// Query query =
+			// em.createQuery(" from CadastroDespesasDAO cd join fetch cd.despesas  join fetch cd.usuario",
+			// CadastroDespesasDAO.class);
+
+			Query query = em.createQuery(
+					" from CadastroDespesasDAO cd join fetch cd.despesas  join fetch cd.usuario"
+							+ " where cd.usuario='" + id + "'"
+							+ " and cd.data BETWEEN'" + data10 + "'and'" +  data11 
+							+ "'" + " ORDER BY '" + data10 + "' DESC ",
+					CadastroDespesasDAO.class);
+			lista = query.getResultList();
+			tx.commit();
+		} catch (Exception e) {
+			System.out.println("erro ao gerar a lista: " + e);
+		}
+		// PersistenceUtil.close(em);
+		// PersistenceUtil.close();
+		return lista;
+
+	}
+	
+	
+	
+	public List<CadastroReceitasDAO> gerenciaListaReceita(Long id,
+			Date dataInicio, Date datafinal) throws ParseException {
+
+		EntityManager em = PersistenceUtil.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		List<CadastroReceitasDAO> lista = null;
+
+		// formatando a data
+		SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd");
+String data10= formato.format(dataInicio);
+		SimpleDateFormat formato1 = new SimpleDateFormat("yyyy/MM/dd");
+String data11=formato1.format(datafinal);
+				
+		try {
+			tx.begin();
+			// Query query =
+			// em.createQuery(" from CadastroDespesasDAO cd join fetch cd.despesas  join fetch cd.usuario",
+			// CadastroDespesasDAO.class);
+
+			Query query = em.createQuery(
+					" from CadastroReceitasDAO cd join fetch cd.receitas  join fetch cd.usuario"
+							+ " where cd.usuario='" + id + "'"
+							+ " and cd.data BETWEEN'" + data10 + "'and'" +  data11 
+							+ "'" + " ORDER BY '" + data10 + "' DESC ",
+					CadastroReceitasDAO.class);
+			lista = query.getResultList();
+			tx.commit();
+		} catch (Exception e) {
+			System.out.println("erro ao gerar a lista: " + e);
+		}
+		// PersistenceUtil.close(em);
+		// PersistenceUtil.close();
+		return lista;
+
+	}
+	
+	
+	
+	
+	public  Double relatorioGeralDespesaTotal(Long id,
+			Date dataInicio, Date datafinal) throws ParseException {
+
+		EntityManager em = PersistenceUtil.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+        
+		
+		Double valorTotalDespesa = null;
+	      
+
+		// formatando a data
+		SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd");
+String data10= formato.format(dataInicio);
+		SimpleDateFormat formato1 = new SimpleDateFormat("yyyy/MM/dd");
+String data11=formato1.format(datafinal);
+				
+		try {
+			tx.begin();
+			// Query query =
+			// em.createQuery(" from CadastroDespesasDAO cd join fetch cd.despesas  join fetch cd.usuario",
+			// CadastroDespesasDAO.class);
+
+			Query query = em.createQuery(
+					"  select sum(cd.valor)from CadastroDespesasDAO cd"
+							+ " where cd.usuario='" + id + "'"
+							+ " and cd.data BETWEEN'" + data10 + "'and'" +  data11 
+							+ "'" + " ORDER BY '" + data10 + "' DESC ",
+					Double.class);
+			valorTotalDespesa = (Double) query.getSingleResult();
+			tx.commit();
+		} catch (Exception e) {
+			System.out.println("erro ao gerar a lista: " + e);
+		}
+		// PersistenceUtil.close(em);
+		// PersistenceUtil.close();
+		
+		
+		if(valorTotalDespesa==null){
+			
+			valorTotalDespesa=0.00;
+		}
+		
+		
+		return valorTotalDespesa;
+
+	}
+	
+	
+	
+	public  Double relatorioGeralreceitaTotal(Long id,
+			Date dataInicio, Date datafinal) throws ParseException {
+
+		EntityManager em = PersistenceUtil.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+        
+		
+		Double valorTotalReceita = null;
+	      
+
+		// formatando a data
+		SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd");
+String data10= formato.format(dataInicio);
+		SimpleDateFormat formato1 = new SimpleDateFormat("yyyy/MM/dd");
+String data11=formato1.format(datafinal);
+				
+		try {
+			tx.begin();
+			// Query query =
+			// em.createQuery(" from CadastroDespesasDAO cd join fetch cd.despesas  join fetch cd.usuario",
+			// CadastroDespesasDAO.class);
+
+			Query query = em.createQuery(
+					"  select sum(cd.valor)from CadastroReceitasDAO cd"
+							+ " where cd.usuario='" + id + "'"
+							+ " and cd.data BETWEEN'" + data10 + "'and'" +  data11 
+							+ "'" + " ORDER BY '" + data10 + "' DESC ",
+					Double.class);
+			valorTotalReceita = (Double) query.getSingleResult();
+			tx.commit();
+		} catch (Exception e) {
+			System.out.println("erro ao gerar a lista: " + e);
+		}
+		// PersistenceUtil.close(em);
+		// PersistenceUtil.close();
+		
+		
+		if(valorTotalReceita==null){
+			
+			valorTotalReceita=0.00;
+		}
+		
+		
+		return valorTotalReceita;
+
+	}
+
+	
+
+}
